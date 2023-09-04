95098db05a1f8293ddfcfa79700ef99834f22418
Rup medhi <rupmmedhi@gmail.com>
2023-09-05 00:31:20 +0530

    conditional rendering in layout

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,14 +1,22 @@
+"use client"
 import Link from "next/link";
 import './about.css'
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }) {
+    const pathName = usePathname()
     return (
         <div>
             <br />
             <div className="layout">
-                 <h2>About Section</h2>
-                <Link href="/about/aboutcollege">Go to About College</Link>
-                <Link href="/about/aboutstudents">Go to About Students</Link>
+                {
+                    pathName !== "/about/aboutcollege" ? <>
+                        <h2>About Section</h2>
+                        <Link href="/about/aboutcollege">Go to About College</Link>
+                        <Link href="/about/aboutstudents">Go to About Students</Link>
+                    </> : null
+                }
+
             </div>
 
             <br />
