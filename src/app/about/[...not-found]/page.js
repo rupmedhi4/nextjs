19ee2a2504e67820e6dda1cf5a30d88ddee10a63
Rup medhi <rupmmedhi@gmail.com>
2023-09-05 11:17:43 +0530

    404 page

---
+++ src/app/about/[...not-found]/page.js
@@ -0,0 +1,9 @@
+
+export default function NotFound() {
+    return (
+      <div>
+        <h1>this about page not found</h1>
+      </div>
+    )
+  }
+  
---
+++ src/app/not-found.js
@@ -0,0 +1,8 @@
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>this page not found</h1>
+    </div>
+  )
+}
