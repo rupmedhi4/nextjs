c6c331535eedb67ad690ef8af69119b1a600f336
Rup medhi <rupmmedhi@gmail.com>
2023-09-05 20:13:01 +0530

    api call in server side & client side

--- src/app/productList/page.js
+++ src/app/productList/page.js
@@ -1,25 +1,48 @@
-"use client"
-import { useEffect, useState } from "react"
+// "use client"
+// import { useEffect, useState } from "react"
 
 
-export default function page() {
-    const [product, setProduct] = useState([])
+// export default function page() {
+//     const [product, setProduct] = useState([])
 
-    useEffect(async () => {
+//     useEffect(async () => {
+//         let data = await fetch("https://dummyjson.com/products")
+//         data = await data.json()
+//         setProduct(data.products)
+
+//     }, [])
+//        console.log(product);
+//     return (
+//         <div>
+//             <h1>product list</h1>
+//             {
+//                 product.map((item) => (
+//                     <h2>product name : {item.title}</h2>
+//                 ))
+//             }
+//         </div>
+//     )
+// }
+
+// SERVER SIDE API CALL
+
+
+
+export default async function page() {
+
+    const apiCall = async () => {
         let data = await fetch("https://dummyjson.com/products")
         data = await data.json()
-        setProduct(data.products)
-     
-    }, [])
-       console.log(product);
+        return data.products
+    }
+    const data = await apiCall()
+    
     return (
         <div>
-            <h1>product list</h1>
-            {
-                product.map((item) => (
-                    <h2>product name : {item.title}</h2>
-                ))
-            }
+            <h1>Product list</h1>
+            {data.map((item) => (
+                <h2>{item.title}</h2>
+            ))}
         </div>
     )
 }
